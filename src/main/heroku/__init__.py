3340c35f51662f4862eb8edb80a12525e35f09bb
VietNguyen <hviet.nguyen88@gmail.com>
2026-06-06 01:15:59 +0300

    Commit

---
+++ src/main/heroku/__init__.py
@@ -0,0 +1,6 @@
+"""heroku package
+Converted to a Python package so it can be imported as src.main.heroku or main.heroku
+"""
+from . import person
+
+__all__ = ["person"]
---
+++ src/test/herokutest/__init__.py
@@ -0,0 +1,3 @@
+# herokutest package
+# Converted to a Python package so test.herokutest can be imported
+__all__ = []
